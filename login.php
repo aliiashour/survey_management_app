1d271cddefe2299ce8a38ce8c5cdb27e542fcdb5
ali_ashour <aliashour592@gmail.com>
2022-11-09 17:57:52 +0200

    modify the user side

--- login.php
+++ login.php
@@ -2,24 +2,32 @@
     $page_title = "login" ; 
     include_once "./init.php" ; 
     $response = '' ;  
-    if(isset($_SESSION['user_id'])){
-        header('location:pendingSurvey/index.php') ; 
+    if(isset($_SESSION['user_id']) && isset($_SESSION['user_type']) && $_SESSION['user_type'] == 'USER'){
+        header('location:pendingSurvey/') ; 
+    }  
+    if(isset($_SESSION['user_id']) && isset($_SESSION['user_type']) && $_SESSION['user_type'] == 'ADMIN'){
+        header('location:admin/') ; 
     }
-    if(isset($_REQUEST['login']) && $_REQUEST['login']='login'){
-        if($_REQUEST['user_email']){
+    if(isset($_POST['login']) && $_POST['login']='login'){
+        if($_POST['user_email']){
             $q = 'SELECT * FROM users WHERE user_email = :user_email' ; 
             $stmt = $con->prepare($q) ; 
             $stmt->execute(array(
-                ':user_email' => $_REQUEST['user_email']
+                ':user_email' => $_POST['user_email']
             )) ; 
             $count = $stmt->rowCount() ; 
             if($count){
                 $res = $stmt->fetch();
-                // if(sha1($_REQUEST["user_password"])==$res['user_password']){
-                if(true){
+                if(sha1($_POST["password"])==$res['user_password']){
                     $_SESSION['user_id'] = $res['user_id'] ; 
                     $_SESSION['user_name'] = $res['user_name'] ; 
-                    header('location:pendingSurvey/') ; 
+                    if($res['user_type'] == 'USER'){
+                        $_SESSION['user_type'] = 'USER' ; 
+                        header('location:pendingSurvey/') ; 
+                    }else{
+                        $_SESSION['user_type'] = 'ADMIN' ; 
+                        header('location:admin/') ; 
+                    }
                 }else{
                     $response = '<div class="alert alert-danger">wrong password</div>' ;     
                 }
@@ -48,7 +56,7 @@
                 </div>
                 <div class="mb-3">
                     <label for="user_password" class="form-label">Enter you password</label>
-                    <input type="password" class="form-control" name="user_password">
+                    <input type="password" class="form-control" name="password">
                 </div>
                 <div class="mb-3 form-check text-center">
                     <span>you have no account?<a href="./register.php">register</a></span>
--- pendingSurvey/index.php
+++ pendingSurvey/index.php
@@ -15,15 +15,25 @@
             <div class="row">
                 <nav class="navbar navbar-expand-lg bg-dark">
                     <div class="container-fluid">
-                    <a class="navbar-brand text-light" href="#">SurveyBuilder</a>
-                    
-                    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                        <ul class="navbar-nav mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link text-light" href="../logout.php">logout</a>
-                            </li>
-                        </ul>
-                    </div>
+                        <a class="navbar-brand text-light" href="./">SurveyBuilder</a>
+                        
+                        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                            <ul class="navbar-nav mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <div class="dropdown">
+                                        <span class="text-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            <?php echo $res['user_email']?>
+                                        </span>
+                                        <ul class="dropdown-menu">
+                                            <li><span id="change_password_button" class="dropdown-item" href="" data-modal="reset_password_modal">change password</span></li>
+                                            <hr>
+                                            <li><a class="dropdown-item" href="../logout.php">logout</a></li>
+                                        </ul>
+                                    </div>
+                                    <!-- <a class="nav-link text-light" href="../logout.php">logout</a> -->
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </nav>
             </div>
@@ -35,7 +45,8 @@
                     <!-- 
                         get all surveys i submit
                         surveys report
-                    -->
+                    -->                    
+                    <div id="response"></div>
                     <table id="datatable" class="table hover">
                         <thead>
                             <th>S.N</th>
@@ -88,9 +99,85 @@
                 window.location.href = new_location ; 
             }
 
+            // open reset_password_modal when clicking the change_password_button
+            $("#change_password_button").on('click', function(){
+                $("#reset_password_modal").modal('show') ; 
+            })
+
+            // submit the modal 
+            $(document).on("submit", "#reset_password_modal", function(event){
+                event.preventDefault() ; 
+                var old_password = $("#old_password").val() ; 
+                var new_password = $("#new_password").val() ; 
+                if(old_password != '' && new_password != ''){
+                    // what id the old == new 
+                    if(old_password==new_password){
+                        $("#response_form").html('<div class="alert alert-danger">can not use the old one as the new password</div>') ; 
+                    }else{
+                        $.ajax({
+                            url:"../inc/handle_files/reset_password.php",
+                            method:"POST",
+                            data:{old_password:old_password, new_password:new_password},
+                            beforeSend:function(){
+                                $("#reset_button").html('wait..');
+                                $("#reset_button").attr('disabled', 'disabled');
+                            },
+                            success:function(data){
+                                var json = JSON.parse(data) ; 
+                                if(json.status == 'true'){
+                                    // successfully change password
+                                    $("#response").html('<div class="alert alert-success">password succefully changed</div>') ;
+                                    var old_password = $("#old_password").val('') ; 
+                                    var new_password = $("#new_password").val('') ; 
+                                    $("#reset_password_modal").modal('hide') ; 
+                                }else{
+                                    // the old one is diffrent
+                                    $("#response_form").html('<div class="alert alert-danger">'+ json.error +'</div>') ; 
+                                }
+                                $("#reset_button").attr('disabled', false);
+                                $("#reset_button").html('Reset');
+                            }
+                        });
+                    }
+                }else{
+                    $("#response_form").html('<div class="alert alert-danger">fill all record</div>') ; 
+                }
+                setTimeout(function(){
+                    $("#response_form").html('');
+                    $("#response").html('');
+                }, 2000);
+
+            });
+
         </script>
     <?php else: ?>
         <p>you should not to be here</p>
     <?php endif ?>
+    <div id="reset_password_modal" class="modal" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Reset Password</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div id="response_form"></div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Old Password</label>
+                        <input type="password" class="form-control" name="old_password" id="old_password">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">New Password</label>
+                        <input type="password" class="form-control" name="new_password" id="new_password">
+                    </div>
+            </div>
+            <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary bg-danger" id="reset_button">Reset</button>
+                </form>
+            </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
