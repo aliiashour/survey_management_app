f889e94d84fb1573ae458d2d346b179fe6a9f031
ali_ashour <aliashour592@gmail.com>
2022-11-25 15:41:50 +0200

    check if user is active before send survey

--- admin/inc/handle_files/surveys/send_survey.php
+++ admin/inc/handle_files/surveys/send_survey.php
@@ -12,37 +12,46 @@
         if($count){
             $res = $stmt->fetch() ;
             $user_id = $res['user_id'] ; 
-            // if the user found i should check if survey active not pended
-            $q = "SELECT * FROM survey WHERE survey_id = :survey_id AND survey_status='active'" ; 
+            // if the user found i should check if he active
+            $q = "SELECT * FROM users WHERE user_id = :user_id AND user_status='active'" ; 
             $stmt = $con->prepare($q) ; 
-            $res = $stmt->execute(array(':survey_id' => $survey_id));
+            $res = $stmt->execute(array(':user_id' => $user_id));
             $count = $stmt->rowCount() ; 
             if($count){
-                // check if it send before
-                $q = "SELECT * FROM survey_info WHERE user_id = :user_id AND survey_id= :survey_id" ; 
+                // if the user found and active i should check if survey active not pended
+                $q = "SELECT * FROM survey WHERE survey_id = :survey_id AND survey_status='active'" ; 
                 $stmt = $con->prepare($q) ; 
-                $res = $stmt->execute(array(
-                    ':user_id' => $user_id,
-                    ':survey_id' => $survey_id,
-                ));
+                $res = $stmt->execute(array(':survey_id' => $survey_id));
                 $count = $stmt->rowCount() ; 
-                if(!$count){
-                    $q = "INSERT INTO survey_info SET
-                        user_id = :user_id,
-                        survey_id = :survey_id 
-                    " ; 
+                if($count){
+                    // check if it send before
+                    $q = "SELECT * FROM survey_info WHERE user_id = :user_id AND survey_id= :survey_id" ; 
                     $stmt = $con->prepare($q) ; 
                     $res = $stmt->execute(array(
+                        ':user_id' => $user_id,
                         ':survey_id' => $survey_id,
-                        ':user_id' => $user_id
-                        )
-                    );
-                    $data = array('status' => 'success', 'msg'=>'Survey Successfully sended') ; 
+                    ));
+                    $count = $stmt->rowCount() ; 
+                    if(!$count){
+                        $q = "INSERT INTO survey_info SET
+                            user_id = :user_id,
+                            survey_id = :survey_id 
+                        " ; 
+                        $stmt = $con->prepare($q) ; 
+                        $res = $stmt->execute(array(
+                            ':survey_id' => $survey_id,
+                            ':user_id' => $user_id
+                            )
+                        );
+                        $data = array('status' => 'success', 'msg'=>'Survey Successfully sended') ; 
+                    }else{
+                        $data = array('status' => 'failed', 'msg'=>'this survey sent already once before') ;     
+                    }
                 }else{
-                    $data = array('status' => 'failed', 'msg'=>'this survey sent already once before') ;     
+                    $data = array('status' => 'failed', 'msg'=>'this survey not active yet') ; 
                 }
             }else{
-                $data = array('status' => 'failed', 'msg'=>'this survey not active yet') ; 
+                $data = array('status' => 'failed', 'msg'=>'this user is pended') ; 
             }
         }else{
             $data = array('status' => 'failed', 'msg'=>'this user email not defined') ; 
